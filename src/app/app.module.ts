0dd6be709a6edc1692074769b3d62bd1281c2f00
Jim Phas <JimPhas@gmail.com>
2022-02-14 13:12:18 -0500

    confirmed changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoryListComponent } from './components/story-list/story-list.component';
+import { EditStoryComponent } from './components/edit-story/edit-story.component';
+import { CreateStoryComponent } from './components/create-story/create-story.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoryListComponent,
+    EditStoryComponent,
+    CreateStoryComponent,
   ],
   imports: [
     BrowserModule,
