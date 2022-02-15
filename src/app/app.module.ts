9660ebdb885202fefb8d9aab2d5cb8ac8cf345e9
Jim Phas <JimPhas@gmail.com>
2022-02-15 13:20:59 -0500

    debugging mdb navbar test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Router, RouterModule, Routes } from '@angular/router';
+
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoryListComponent } from './components/story-list/story-list.component';
 import { EditStoryComponent } from './components/edit-story/edit-story.component';
 import { CreateStoryComponent } from './components/create-story/create-story.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'homepage', pathMatch: 'full' },
+  { path: 'homepage', component: HomepageComponent },
+  { path: 'storylist', component: StoryListComponent },
+  { path: 'create', component: CreateStoryComponent },
+  { path: 'edit',component: EditStoryComponent }
+]
 
 @NgModule({
   declarations: [
@@ -13,11 +28,18 @@ import { CreateStoryComponent } from './components/create-story/create-story.com
     StoryListComponent,
     EditStoryComponent,
     CreateStoryComponent,
+    HomepageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    NgbModule,
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
