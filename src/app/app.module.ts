f3d3c40bb39f37d80c7103b0d808343c2ca363e2
Jim Phas <JimPhas@gmail.com>
2022-02-16 10:42:00 -0500

    figured out app components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,7 +29,7 @@ const routes: Routes = [
     EditStoryComponent,
     CreateStoryComponent,
     HomepageComponent,
-    NavbarComponent
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
